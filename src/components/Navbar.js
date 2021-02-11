8309783c22f95e9772c8fd6bcbc8a525fd43d273
fatiho <fatiho@seas.upenn.edu>
2021-02-11 18:30:17 -0500

    Navbar complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,8 @@
 import React from "react"
 import logo from "../logo.png" 
+// REACT FONTAWESOME IMPORTS
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faBars} from "@fortawesome/free-solid-svg-icons"
 
 const Navbar = () => {
     return (
@@ -8,7 +11,7 @@ const Navbar = () => {
 
   <a className="navbar-brand" href="#">Fatih Ozer</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
+    <FontAwesomeIcon icon={faBars} style={{color: "#fff"}}/>
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
